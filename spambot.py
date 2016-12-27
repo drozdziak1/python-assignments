630db18e62b592ef0f76f86b3ac0330daa13f81d
Stanisław Drozd <drozdziak1@gmail.com>
2016-12-27 21:34:12 +0100

    finish the overkill spambot script

--- spambot.py
+++ spambot.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Stanisław Drozd <drozdziak1@gmail.com>
+
 import argparse
 import urllib3, certifi
 import re
-from sys import stderr # Rid "|" users of garbage in stdout
+import sys # Rid "|" users of garbage in stdout
 
 parser = argparse.ArgumentParser(
         description="Extract e-mail addresses from a website.")
 
-parser.add_argument("--url", metavar="url",
+parser.add_argument(
+        "-u",
+        metavar="url",
         type=str,
         help="crawl through url specifically",
-        default="https://www.wmi.amu.edu.pl/pl/pracownicy"
+        default="http://vps276855.ovh.net/", # A test setup on my private VPS
+        dest="url",
         )
 
-# TODO
-# parser.add_argument("-o", metavar="output_file",
-        # type=str,
-        # help="save to output_file",
-        # )
+parser.add_argument(
+        "-o",
+        metavar="output_file",
+        type=str,
+        help="save to output_file (stdout by default)",
+        dest="output_file",
+        )
 
 
 args = parser.parse_args()
+
+if args.output_file:
+    fout = open(args.output_file, "w");
+else:
+    fout = sys.stdout
 
 http = urllib3.PoolManager(
         cert_reqs = "CERT_REQUIRED",
@@ -32,11 +44,11 @@ http = urllib3.PoolManager(
 try:
     request = http.request("GET", args.url)
 except urllib3.exceptions.MaxRetryError:
-    stderr.write("HTTP request failed.\n")
+    sys.stderr.write("HTTP request failed.\n")
     exit(1)
 
 if request.status != 200:
-    stderr.write("HTTP request failed with status No. %d\n" % request.status)
+    sys.stderr.write("HTTP request failed with status No. %d\n" % request.status)
     exit(1)
 
 # Should have probably used the email module
@@ -48,15 +60,17 @@ emails = searchPattern.findall(request.data.decode(errors="ignore"))
 count = len(emails)
 
 if(count >= 2):
-    stderr.write("Found %d Addresses:\n" % count)
+    sys.stderr.write("Found %d addresses.\n" % count)
 elif(count):
-    stderr.write("Found 1 Address:\n")
+    sys.stderr.write("Found 1 address.\n")
 else:
-    stderr.write("No e-mail addresses found.\n")
-    # Following grep's "no matches -> non-zero exit" convention
-    exit(1)
+    sys.stderr.write("No e-mail addresses found.\n")
+    exit(1) # Following grep's "no matches -> non-zero exit" convention
 
-stderr.flush()
+sys.stderr.flush()
 
 for email in emails:
-    print("%s...%s@%s"% (email[0][0], email[0][-1], email[1]))
+    fout.write("%s...%s@%s\n"% (email[0][0], email[0][-1], email[1]))
+
+if args.output_file:
+    fout.close()
