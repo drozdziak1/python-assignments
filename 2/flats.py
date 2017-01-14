2d1c3418ed1de4d801a24873c484366d909ac1e1
Stanisław Drozd <drozdziak1@gmail.com>
2017-01-14 12:03:57 +0100

    Finish real estate CSV task

---
+++ 2/flats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Stanisław Drozd <drozdziak1@gmail.com>
+
+import pandas
+
+df = pandas.read_csv("http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv")
+
+ad1 = df[df.beds > 4].sort("beds", ascending=0) #desc order for easier checking
+print("Ad1:\n", ad1[["street", "city", "zip", "state", "beds"]])
+
+ad2 = df["price"].sum()
+print("Ad2: $%d" % ad2)
+
+ad3 = df[df.sq__ft < 900].sort("sq__ft", ascending=1)
+print("Ad3:\n", ad3[["street", "city", "zip", "state", "sq__ft"]][1:])
